19abca4648f9de5530622d8277767b1ace0661c2
Anastasis Karniatis <58403196+AnKGaming19@users.noreply.github.com>
2025-12-06 01:06:14 +0200

    Add bottom wave SVG to Hero component
    Added a bottom wave SVG to the Hero component.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -62,6 +62,13 @@ const Hero: React.FC = () => {
       </div>
 
       <ParticleBackground />
+      
+      {/* Bottom Wave */}
+      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none z-20 pointer-events-none">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="block w-full h-[60px] md:h-[100px] rotate-180">
+            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" fill="#0b0c15"></path>
+        </svg>
+      </div>
 
       <div className="container mx-auto px-6 relative z-10 grid lg:grid-cols-2 gap-16 items-center">
         
@@ -227,13 +234,6 @@ const Hero: React.FC = () => {
           </motion.div>
         </div>
       </div>
-      
-      {/* Bottom Wave to Services (#0b0c15) */}
-<div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none z-20">
-<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="block w-full h-[60px] md:h-[100px] rotate-180">
-<path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" fill="#0b0c15"></path>
-</svg>
-</div>
     </section>
   );
 };
